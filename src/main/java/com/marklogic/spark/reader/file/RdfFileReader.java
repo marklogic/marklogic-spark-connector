8a0054111dde5abf618ef4187ba40014b437d105
Rob Rudin <rudin@progress.com>
2024-02-23 14:53:27 -0500

    MLE-12397 Can now read all RDF file types

--- src/main/java/com/marklogic/spark/reader/file/RdfFileReader.java
+++ src/main/java/com/marklogic/spark/reader/file/RdfFileReader.java
@@ -6,6 +6,7 @@ import org.apache.hadoop.fs.Path;
 import org.apache.jena.graph.Triple;
 import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFParserBuilder;
+import org.apache.jena.riot.RiotException;
 import org.apache.jena.riot.system.AsyncParser;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.connector.read.PartitionReader;
@@ -23,22 +24,24 @@ class RdfFileReader implements PartitionReader<InternalRow> {
 
     private final InputStream inputStream;
     private final Iterator<Triple> tripleStream;
+    private final FilePartition partition;
 
     private final RdfSerializer rdfSerializer = new RdfSerializer();
 
     RdfFileReader(FilePartition partition, SerializableConfiguration hadoopConfiguration) {
+        this.partition = partition;
         if (logger.isDebugEnabled()) {
-            logger.debug("Reading RDF file {}", partition.getPath());
+            logger.debug("Reading RDF file: {}", partition.getPath());
         }
         Path path = new Path(partition.getPath());
         try {
             this.inputStream = path.getFileSystem(hadoopConfiguration.value()).open(path);
-            this.tripleStream = AsyncParser.of(RDFParserBuilder.create()
+            RDFParserBuilder parserBuilder = RDFParserBuilder.create()
                 .source(this.inputStream)
-                .lang(Lang.RDFXML)
                 .errorHandler(new RdfErrorHandler(partition.getPath()))
-                .base(partition.getPath())
-            ).streamTriples().iterator();
+                .lang(determineLang(partition))
+                .base(path.getName());
+            this.tripleStream = AsyncParser.of(parserBuilder).streamTriples().iterator();
         } catch (Exception e) {
             throw new ConnectorException(String.format("Unable to read RDF file at %s; cause: %s", path, e.getMessage()), e);
         }
@@ -46,7 +49,16 @@ class RdfFileReader implements PartitionReader<InternalRow> {
 
     @Override
     public boolean next() throws IOException {
-        return this.tripleStream.hasNext();
+        try {
+            return this.tripleStream.hasNext();
+        } catch (RiotException e) {
+            if (e.getMessage().contains("Failed to determine the RDF syntax")) {
+                throw new ConnectorException(String.format("Unable to read RDF file at %s; RDF syntax is not supported or " +
+                    "the file extension is not recognized.", partition.getPath()), e);
+            }
+            throw new ConnectorException(String.format("Unable to read RDF file at %s; cause: %s",
+                partition.getPath(), e.getMessage()), e);
+        }
     }
 
     @Override
@@ -57,5 +69,21 @@ class RdfFileReader implements PartitionReader<InternalRow> {
     @Override
     public void close() throws IOException {
         IOUtils.closeQuietly(this.inputStream);
+    }
+
+    /**
+     * This is only defining extensions that Jena does not appear to recognize. Testing has shown that providing the
+     * file path for the Jena {@code base} method will work for all the file types we support - except for RDF JSON,
+     * we need to Jena that ".json" maps to RDF JSON.
+     *
+     * @param partition
+     * @return
+     */
+    private Lang determineLang(FilePartition partition) {
+        String path = partition.getPath().toLowerCase();
+        if (path.endsWith(".json")) {
+            return Lang.RDFJSON;
+        }
+        return null;
     }
 }
