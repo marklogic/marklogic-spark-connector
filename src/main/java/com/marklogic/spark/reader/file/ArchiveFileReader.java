4877e11880da33b1fa12f305994e448a24b75450
asinha <ansinha@progress.com>
2024-03-29 15:18:13 -0700

    MLE-12958 : Read subset of metadata from NT archive

--- src/main/java/com/marklogic/spark/reader/file/ArchiveFileReader.java
+++ src/main/java/com/marklogic/spark/reader/file/ArchiveFileReader.java
@@ -2,6 +2,7 @@ package com.marklogic.spark.reader.file;
 
 import com.marklogic.client.io.DocumentMetadataHandle;
 import com.marklogic.spark.ConnectorException;
+import com.marklogic.spark.Options;
 import com.marklogic.spark.reader.document.DocumentRowSchema;
 import org.apache.spark.sql.catalyst.InternalRow;
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow;
@@ -12,6 +13,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
@@ -20,40 +23,53 @@ class ArchiveFileReader implements PartitionReader<InternalRow> {
     private final String path;
     private final ZipInputStream zipInputStream;
     private ZipEntry currentZipEntry;
+    private final List<String> metadataCategories;
+    private InternalRow nextRowToReturn;
 
     public ArchiveFileReader(FilePartition partition, FileContext fileContext) {
         this.path = partition.getPath();
         this.zipInputStream = new ZipInputStream(fileContext.open(partition));
+        this.metadataCategories = new ArrayList<>();
+        if (fileContext.hasOption(Options.READ_ARCHIVES_CATEGORIES)) {
+            for (String category : fileContext.getStringOption(Options.READ_ARCHIVES_CATEGORIES).split(",")) {
+                this.metadataCategories.add(category.toLowerCase());
+            }
+        }
     }
 
     @Override
     public boolean next() throws IOException {
         currentZipEntry = FileUtil.findNextFileEntry(zipInputStream);
-        return currentZipEntry != null;
-    }
-
-    @Override
-    public InternalRow get() {
-
+        if(currentZipEntry == null)
+            return false;
         byte[] content = readZipEntry();
+        if(content.length == 0)
+            return false;
         String zipEntryName = currentZipEntry.getName();
+        DocumentMetadataHandle documentMetadataHandle = new DocumentMetadataHandle();
         if (logger.isTraceEnabled()) {
             logger.trace("Reading zip entry {} from zip file {}.", zipEntryName, this.path);
         }
         String uri = zipEntryName.startsWith("/") ? this.path + zipEntryName : this.path + "/" + zipEntryName;
         try {
             ZipEntry metadataEntry = FileUtil.findNextFileEntry(zipInputStream);
-            if (metadataEntry == null || !metadataEntry.getName().endsWith(".metadata")) {
+            if(metadataEntry == null)
+                return false;
+            if(!metadataEntry.getName().endsWith(".metadata")) {
                 throw new ConnectorException(String.format("Could not find metadata entry for entry %s", zipEntryName));
             }
+            documentMetadataHandle.fromBuffer(readZipEntry());
         } catch (IOException e) {
             throw new ConnectorException(String.format("Unable to read zip file at %s; cause: %s", this.path, e.getMessage()), e);
         }
 
-        DocumentMetadataHandle documentMetadataHandle = new DocumentMetadataHandle();
-        documentMetadataHandle.fromBuffer(readZipEntry());
+        this.nextRowToReturn = makeRow(uri, content,documentMetadataHandle);
+        return true;
+    }
 
-        return makeRow(uri, content, documentMetadataHandle);
+    @Override
+    public InternalRow get() {
+        return nextRowToReturn;
     }
 
     @Override
@@ -78,7 +94,43 @@ class ArchiveFileReader implements PartitionReader<InternalRow> {
         if (documentMetadataHandle.getFormat() != null) {
             row[2] = UTF8String.fromString(documentMetadataHandle.getFormat().name());
         }
-        DocumentRowSchema.populateAllMetadata(row, documentMetadataHandle);
+        if(this.metadataCategories.isEmpty()) {
+            populateAllMetadata(row, documentMetadataHandle);
+        }
+        else
+            populateMetadataCategories(row, documentMetadataHandle, metadataCategories);
         return new GenericInternalRow(row);
+    }
+
+    private void populateAllMetadata(Object[] row, DocumentMetadataHandle documentMetadataHandle) {
+        DocumentRowSchema.populateCollectionsColumn(row, documentMetadataHandle);
+        DocumentRowSchema.populatePermissionsColumn(row, documentMetadataHandle);
+        DocumentRowSchema.populateQualityColumn(row, documentMetadataHandle);
+        DocumentRowSchema.populatePropertiesColumn(row, documentMetadataHandle);
+        DocumentRowSchema.populateMetadataValuesColumn(row, documentMetadataHandle);
+    }
+
+    private void populateMetadataCategories(Object[] row, DocumentMetadataHandle documentMetadataHandle,
+                                            List<String> metadataCategories) {
+        if (categoryIsIncluded("collections", metadataCategories)) {
+            DocumentRowSchema.populateCollectionsColumn(row, documentMetadataHandle);
+        }
+
+        if (categoryIsIncluded("permissions", metadataCategories)) {
+            DocumentRowSchema.populatePermissionsColumn(row, documentMetadataHandle);
+        }
+        if (categoryIsIncluded("quality", metadataCategories)) {
+            DocumentRowSchema.populateQualityColumn(row, documentMetadataHandle);
+        }
+        if (categoryIsIncluded("properties", metadataCategories)) {
+            DocumentRowSchema.populatePropertiesColumn(row, documentMetadataHandle);
+        }
+        if (categoryIsIncluded("metadatavalues", metadataCategories)) {
+            DocumentRowSchema.populateMetadataValuesColumn(row, documentMetadataHandle);
+        }
+    }
+
+    private boolean categoryIsIncluded(String category, List<String> metadataCategories) {
+        return metadataCategories.contains(category);
     }
 }
--- src/test/java/com/marklogic/spark/reader/file/ReadArchiveFileTest.java
+++ src/test/java/com/marklogic/spark/reader/file/ReadArchiveFileTest.java
@@ -26,7 +26,7 @@ class ReadArchiveFileTest extends AbstractIntegrationTest {
     }
 
     @Test
-    void test(@TempDir Path tempDir) {
+    void testAllMetadata(@TempDir Path tempDir) {
         newSparkSession().read().format(CONNECTOR_IDENTIFIER)
             .option(Options.CLIENT_URI, makeClientUri())
             .option(Options.READ_DOCUMENTS_COLLECTIONS, "collection1")
@@ -38,10 +38,104 @@ class ReadArchiveFileTest extends AbstractIntegrationTest {
             .mode(SaveMode.Append)
             .save(tempDir.toFile().getAbsolutePath());
 
-        verifyFileRows(tempDir, 2);
+        verifyAllMetadata(tempDir, 2);
     }
 
-    private void verifyFileRows(Path tempDir, int rowCount) {
+    @Test
+    void testCollectionsAndPermissions(@TempDir Path tempDir) {
+        newSparkSession().read().format(CONNECTOR_IDENTIFIER)
+            .option(Options.CLIENT_URI, makeClientUri())
+            .option(Options.READ_DOCUMENTS_COLLECTIONS, "collection1")
+            .option(Options.READ_DOCUMENTS_CATEGORIES, "content,metadata")
+            .load()
+            .repartition(1)
+            .write().format(CONNECTOR_IDENTIFIER)
+            .option(Options.WRITE_FILES_COMPRESSION, "zip")
+            .option(Options.WRITE_FILES_COMPRESSION, "zip")
+            .mode(SaveMode.Append)
+            .save(tempDir.toFile().getAbsolutePath());
+
+        List<Row>  rows =  newSparkSession().read()
+            .format(CONNECTOR_IDENTIFIER)
+            .option(Options.READ_FILES_TYPE, "archive")
+            .option(Options.READ_ARCHIVES_CATEGORIES, "collections,permissions")
+            .load(tempDir.toFile().getAbsolutePath())
+            .collectAsList();
+        assertEquals(2, rows.size(), "Expecting 2 rows in the zip.");
+        rows.forEach(row ->{
+            verifyContent(row);
+            assertEquals("XML", row.get(2));
+            verifyCollections(row);
+            verifyPermissions(row);
+            assertNull(row.get(5),"Quality column should be null.");
+            assertNull(row.get(6),"Properties column should be null.");
+            assertNull(row.get(7),"MetadataValues column should be null.");
+        });
+    }
+
+    @Test
+    void testQualityPropertiesAndMetadatavalues(@TempDir Path tempDir) {
+        newSparkSession().read().format(CONNECTOR_IDENTIFIER)
+            .option(Options.CLIENT_URI, makeClientUri())
+            .option(Options.READ_DOCUMENTS_COLLECTIONS, "collection1")
+            .option(Options.READ_DOCUMENTS_CATEGORIES, "content,metadata")
+            .load()
+            .repartition(1)
+            .write().format(CONNECTOR_IDENTIFIER)
+            .option(Options.WRITE_FILES_COMPRESSION, "zip")
+            .mode(SaveMode.Append)
+            .save(tempDir.toFile().getAbsolutePath());
+
+        List<Row>  rows =  newSparkSession().read()
+            .format(CONNECTOR_IDENTIFIER)
+            .option(Options.READ_FILES_TYPE, "archive")
+            .option(Options.READ_ARCHIVES_CATEGORIES, "quality,properties,metadataValues")
+            .load(tempDir.toFile().getAbsolutePath())
+            .collectAsList();
+        assertEquals(2, rows.size(), "Expecting 2 rows in the zip.");
+        rows.forEach(row ->{
+            verifyContent(row);
+            assertEquals("XML", row.get(2));
+            assertNull(row.get(3),"Collections column should be null.");
+            assertNull(row.get(4),"Permissions column should be null.");
+            assertEquals(10, row.get(5));
+            verifyProperties(row);
+            verifyMetadatavalues(row);
+        });
+    }
+
+    @Test
+    void testCollectionsAndMetadatavalues(@TempDir Path tempDir) {
+        newSparkSession().read().format(CONNECTOR_IDENTIFIER)
+            .option(Options.CLIENT_URI, makeClientUri())
+            .option(Options.READ_DOCUMENTS_COLLECTIONS, "collection1")
+            .option(Options.READ_DOCUMENTS_CATEGORIES, "content,metadata")
+            .load()
+            .repartition(1)
+            .write().format(CONNECTOR_IDENTIFIER)
+            .option(Options.WRITE_FILES_COMPRESSION, "zip")
+            .mode(SaveMode.Append)
+            .save(tempDir.toFile().getAbsolutePath());
+
+        List<Row>  rows =  newSparkSession().read()
+            .format(CONNECTOR_IDENTIFIER)
+            .option(Options.READ_FILES_TYPE, "archive")
+            .option(Options.READ_ARCHIVES_CATEGORIES, "collections,metadataValues")
+            .load(tempDir.toFile().getAbsolutePath())
+            .collectAsList();
+        assertEquals(2, rows.size(), "Expecting 2 rows in the zip.");
+        rows.forEach(row ->{
+            verifyContent(row);
+            assertEquals("XML", row.get(2));
+            verifyCollections(row);
+            assertNull(row.get(4),"Permissions column should be null.");
+            assertNull(row.get(5),"Quality column should be null.");
+            assertNull(row.get(6),"Properties column should be null.");
+            verifyMetadatavalues(row);
+        });
+    }
+
+    private void verifyAllMetadata(Path tempDir, int rowCount) {
         List<Row> rows = sparkSession.read().format(CONNECTOR_IDENTIFIER)
             .option(Options.READ_FILES_TYPE, "archive")
             .load(tempDir.toFile().getAbsolutePath())
@@ -51,30 +145,43 @@ class ReadArchiveFileTest extends AbstractIntegrationTest {
         for(int i=0; i<rowCount; i++){
             Row row = rows.get(i);
             assertTrue(row.getString(0).endsWith("/test/"+(i+1)+".xml"));
-
-            String content = new String((byte[]) row.get(1), StandardCharsets.UTF_8);
-            assertTrue(content.contains("<hello>world</hello>"));
-
+            verifyContent(row);
             assertEquals("XML", row.get(2));
-
-            List<String> collections = JavaConversions.seqAsJavaList(row.getSeq(3));
-            assertEquals("collection1", collections.get(0));
-            assertEquals("collection2", collections.get(1));
-
-            Map<String, WrappedArray> permissions = row.getJavaMap(4);
-            assertTrue(permissions.get("spark-user-role").toString().contains("READ"));
-            assertTrue(permissions.get("spark-user-role").toString().contains("UPDATE"));
-            assertTrue(permissions.get("qconsole-user").toString().contains("READ"));
-
-            assertEquals(10, row.get(5));
-
-            Map<String, String> properties = row.getJavaMap(6);
-            assertEquals("value2", properties.get("key2"));
-            assertEquals("value1", properties.get("{org:example}key1"));
-
-            Map<String, String> metadataValues = row.getJavaMap(7);
-            assertEquals("value1", metadataValues.get("meta1"));
-            assertEquals("value2", metadataValues.get("meta2"));
+            verifyCollections(row);
+            verifyPermissions(row);
+            assertEquals(row.get(5),10);
+            verifyProperties(row);
+            verifyMetadatavalues(row);
         }
+    }
+
+    private void verifyContent(Row row){
+        String content = new String((byte[]) row.get(1), StandardCharsets.UTF_8);
+        assertTrue(content.contains("<hello>world</hello>"));
+    }
+
+    private void verifyCollections(Row row){
+        List<String> collections = JavaConversions.seqAsJavaList(row.getSeq(3));
+        assertEquals("collection1", collections.get(0));
+        assertEquals("collection2", collections.get(1));
+    }
+
+    private void verifyPermissions(Row row){
+        Map<String, WrappedArray> permissions = row.getJavaMap(4);
+        assertTrue(permissions.get("spark-user-role").toString().contains("READ"));
+        assertTrue(permissions.get("spark-user-role").toString().contains("UPDATE"));
+        assertTrue(permissions.get("qconsole-user").toString().contains("READ"));
+    }
+
+    private void verifyProperties(Row row){
+        Map<String, String> properties = row.getJavaMap(6);
+        assertEquals("value2", properties.get("key2"));
+        assertEquals("value1", properties.get("{org:example}key1"));
+    }
+
+    private void verifyMetadatavalues(Row row){
+        Map<String, String> metadataValues = row.getJavaMap(7);
+        assertEquals("value1", metadataValues.get("meta1"));
+        assertEquals("value2", metadataValues.get("meta2"));
     }
 }
