49c0e8c1439fd3ff7d9c694c92adee552dde0907
Rob Rudin <rudin@progress.com>
2024-06-03 15:45:45 -0400

    Extracted connection string validation for reuse in NT
    No functional changes, just refactoring.

---
+++ src/main/java/com/marklogic/spark/ConnectionString.java
@@ -0,0 +1,83 @@
+package com.marklogic.spark;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+public class ConnectionString {
+
+    private final String host;
+    private final int port;
+    private final String username;
+    private final String password;
+    private final String database;
+
+    public ConnectionString(String connectionString, String optionNameForErrorMessage) {
+        final String errorMessage = String.format(
+            "Invalid value for %s; must be username:password@host:port/optionalDatabaseName",
+            optionNameForErrorMessage
+        );
+
+        String[] parts = connectionString.split("@");
+        if (parts.length != 2) {
+            throw new ConnectorException(errorMessage);
+        }
+        String[] tokens = parts[0].split(":");
+        if (tokens.length != 2) {
+            throw new ConnectorException(errorMessage);
+        }
+        this.username = decodeValue(tokens[0], "username");
+        this.password = decodeValue(tokens[1], "password");
+
+        tokens = parts[1].split(":");
+        if (tokens.length != 2) {
+            throw new ConnectorException(errorMessage);
+        }
+        this.host = tokens[0];
+        if (tokens[1].contains("/")) {
+            tokens = tokens[1].split("/");
+            this.port = parsePort(tokens[0], optionNameForErrorMessage);
+            this.database = tokens[1];
+        } else {
+            this.port = parsePort(tokens[1], optionNameForErrorMessage);
+            this.database = null;
+        }
+    }
+
+    private int parsePort(String value, String optionNameForErrorMessage) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException e) {
+            throw new ConnectorException(String.format(
+                "Invalid value for %s; port must be numeric, but was '%s'", optionNameForErrorMessage, value, value
+            ));
+        }
+    }
+
+    private String decodeValue(String value, String label) {
+        try {
+            return URLDecoder.decode(value, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new ConnectorException(String.format("Unable to decode '%s'; cause: %s", label, value));
+        }
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+}
