bf66adabe177f58d5034241a2b71f7d87222149f
Rob Rudin <rudin@progress.com>
2024-06-27 15:59:12 -0400

    Renamed encoding test file
    Jenkins is trying to read the .xml file for its own (and unknown) purpose, and it fails since Jenkins is using UTF-8. Renamed the file to end with .txt instead, which has no impact on the tests.

--- src/test/java/com/marklogic/spark/reader/file/ReadAggregateXmlFilesTest.java
+++ src/test/java/com/marklogic/spark/reader/file/ReadAggregateXmlFilesTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ReadAggregateXmlFilesTest extends AbstractIntegrationTest {
 
-    private static final String ISO_8859_1_ENCODED_FILE = "src/test/resources/encoding/medline.iso-8859-1.xml";
+    private static final String ISO_8859_1_ENCODED_FILE = "src/test/resources/encoding/medline.iso-8859-1.txt";
 
     @Test
     void noNamespace() {
