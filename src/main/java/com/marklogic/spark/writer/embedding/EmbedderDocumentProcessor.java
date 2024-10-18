140702bab72c20f30a30759db59a30c18f265f74
Rob Rudin <rudin@progress.com>
2024-10-18 07:55:14 -0400

    Azure hacking

---
+++ src/main/java/com/marklogic/spark/writer/embedding/EmbedderDocumentProcessor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2024 MarkLogic Corporation. All Rights Reserved.
+ */
+package com.marklogic.spark.writer.embedding;
+
+import com.marklogic.client.document.DocumentWriteOperation;
+import com.marklogic.spark.writer.DocumentProcessor;
+import dev.langchain4j.data.document.Metadata;
+import dev.langchain4j.data.embedding.Embedding;
+import dev.langchain4j.data.segment.TextSegment;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.output.Response;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class EmbedderDocumentProcessor implements DocumentProcessor, Supplier<Iterator<DocumentWriteOperation>> {
+
+    private static final Metadata EMPTY_METADATA = new Metadata();
+
+    private final ChunkSelector chunkSelector;
+    private final EmbeddingModel embeddingModel;
+
+    private final int batchSize;
+    private List<Chunk> chunks = new ArrayList();
+    private List<DocumentWriteOperation> currentBatch = new ArrayList<>();
+
+    public EmbedderDocumentProcessor(EmbeddingModel embeddingModel, int batchSize) {
+        this.chunkSelector = new JsonChunkSelector();
+        this.embeddingModel = embeddingModel;
+        this.batchSize = batchSize;
+    }
+
+    @Override
+    public Iterator<DocumentWriteOperation> apply(DocumentWriteOperation sourceDocument) {
+        if (chunks.size() >= batchSize) {
+            currentBatch.clear();
+            chunks.clear();
+        }
+        currentBatch.add(sourceDocument);
+        chunkSelector.selectChunks(sourceDocument).forEachRemaining(chunks::add);
+        return chunks.size() >= batchSize ? get() : Stream.<DocumentWriteOperation>empty().iterator();
+    }
+
+    @Override
+    public Iterator<DocumentWriteOperation> get() {
+        List<TextSegment> segments = new ArrayList<>();
+        chunks.forEach(chunk -> segments.add(new TextSegment(chunk.getEmbeddingText(), EMPTY_METADATA)));
+        System.out.println("CALLING: " + segments.size());
+        Response<List<Embedding>> response = embeddingModel.embedAll(segments);
+        System.out.println("TOKEN USAGE: " + response.tokenUsage());
+        for (int i = 0; i < response.content().size(); i++) {
+            Chunk chunk = chunks.get(i);
+            chunk.addEmbedding(response.content().get(i));
+        }
+        return currentBatch.iterator();
+    }
+}
--- src/test/java/com/marklogic/spark/writer/embedding/EmbeddingTest.java
+++ src/test/java/com/marklogic/spark/writer/embedding/EmbeddingTest.java
@@ -15,28 +15,85 @@ import com.marklogic.spark.writer.splitter.ChunkConfig;
 import com.marklogic.spark.writer.splitter.DefaultChunkAssembler;
 import com.marklogic.spark.writer.splitter.JsonPointerTextSelector;
 import com.marklogic.spark.writer.splitter.SplitterDocumentProcessor;
+import dev.langchain4j.azure.openai.spring.AutoConfig;
 import dev.langchain4j.data.document.splitter.DocumentSplitters;
-import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.model.azure.AzureOpenAiEmbeddingModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2q.AllMiniLmL6V2QuantizedEmbeddingModel;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 class EmbeddingTest extends AbstractIntegrationTest {
+
+    @Autowired
+    private Environment environment;
 
     @Test
     void test() {
         DocumentWriteOperation sourceDoc = readJsonDocument();
-        DocumentProcessor splitter = newJsonSplitter("/text");
+        SplitterDocumentProcessor splitter = newJsonSplitter("/text");
         Iterator<DocumentWriteOperation> docs = splitter.apply(sourceDoc);
 
-        // Could make this batch up requests?
-        DocumentProcessor embedder = new EmbeddingDocumentProcessor(
-            new AllMiniLmL6V2EmbeddingModel()
+        DocumentProcessor embedder = new EmbedderDocumentProcessor(
+            new AllMiniLmL6V2QuantizedEmbeddingModel(), 2
         );
         docs.forEachRemaining(doc -> embedder.apply(doc));
 
         JsonNode doc = JsonUtil.getJsonFromHandle(sourceDoc.getContent());
         System.out.println(doc.toPrettyString());
+    }
+
+    /**
+     * We could have both Java 11 and Java 17 uber jars for Azure support, where the Java 17 one uses
+     * the Spring Boot integration to load config from a properties file.
+     */
+    @Test
+    void azure() {
+        DocumentWriteOperation sourceDoc = readJsonDocument();
+        DocumentProcessor splitter = newJsonSplitter("/text");
+        Iterator<DocumentWriteOperation> docs = splitter.apply(sourceDoc);
+
+        EmbeddingModel embeddingModel = AzureOpenAiEmbeddingModel.builder()
+            .apiKey(environment.getProperty("azureApiKey"))
+            .deploymentName("text-test-embedding-ada-002")
+            .endpoint("https://gpt-testing-custom-data1.openai.azure.com")
+            .logRequestsAndResponses(true)
+            .build();
+
+        List<DocumentWriteOperation> output = new ArrayList<>();
+
+        EmbedderDocumentProcessor embedder = new EmbedderDocumentProcessor(embeddingModel, 2);
+        docs.forEachRemaining(doc -> embedder.apply(doc).forEachRemaining(output::add));
+        embedder.get().forEachRemaining(output::add);
+
+        output.forEach(doc -> {
+            System.out.println(doc.getUri());
+        });
+    }
+
+    @Test
+    void azureSpringBoot() {
+        DocumentWriteOperation sourceDoc = readJsonDocument();
+        DocumentProcessor splitter = newJsonSplitter("/text");
+        Iterator<DocumentWriteOperation> docs = splitter.apply(sourceDoc);
+
+        List<DocumentWriteOperation> output = new ArrayList<>();
+
+        ConfigurableApplicationContext context = SpringApplication.run(AutoConfig.class);
+        EmbeddingModel azureEmbeddingModel = context.getBean(EmbeddingModel.class);
+
+        EmbedderDocumentProcessor embedder = new EmbedderDocumentProcessor(azureEmbeddingModel, 2);
+        docs.forEachRemaining(doc -> embedder.apply(doc).forEachRemaining(output::add));
+        embedder.get().forEachRemaining(output::add);
+
+        output.forEach(doc -> System.out.println(doc.getUri()));
     }
 
     private DocumentWriteOperation readJsonDocument() {
@@ -46,11 +103,14 @@ class EmbeddingTest extends AbstractIntegrationTest {
         return new DocumentWriteOperationImpl(uri, metadata, contentHandle);
     }
 
-    private DocumentProcessor newJsonSplitter(String... jsonPointers) {
+    private SplitterDocumentProcessor newJsonSplitter(String... jsonPointers) {
         return new SplitterDocumentProcessor(
             new JsonPointerTextSelector(jsonPointers, null),
             DocumentSplitters.recursive(500, 0),
-            new DefaultChunkAssembler(new ChunkConfig.Builder().build())
+            new DefaultChunkAssembler(new ChunkConfig.Builder()
+                .withMaxChunks(2)
+                // TODO The rootName screws up the default path.
+                .build())
         );
     }
 }
