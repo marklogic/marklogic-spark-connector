8b42d4b0613083c94f4741392010fc017e0ce619
Rob Rudin <rudin@progress.com>
2024-11-26 10:03:14 -0500

    Tweaked calculation of buckets per partition
    While researching the effects of batch size = zero, I realized that it would be better for the number of buckets per partition to be ceiling'ed to minimize the chance that any bucket has more rows than `batchSize`.
    
    Added a test to verify the low-level functionality for calculating the number of partitions and buckets based on row count, the user-defined partition count, and the batch size.

---
+++ marklogic-spark-connector/src/test/java/com/marklogic/spark/reader/optic/CalculatePartitionsTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright © 2024 MarkLogic Corporation. All Rights Reserved.
+ */
+package com.marklogic.spark.reader.optic;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatePartitionsTest {
+
+    @ParameterizedTest
+    @CsvSource({
+        "1,0,1,1",
+        "2,0,2,2",
+        "1,5000,1,2",
+        "1,5001,1,2",
+        "1,6666,1,2",
+        "1,6667,1,2",
+        "1,9999,1,2",
+        "1,10000,1,1",
+        "1,10001,1,1",
+        "3,3000,3,6"
+    })
+    void test(long userPartitionCount, long batchSize, int expectedPartitionCount, int expectedBucketCount) {
+        long rowCount = 10000;
+        List<PlanAnalysis.Partition> partitions = PlanAnalyzer.calculatePartitions(rowCount, userPartitionCount, batchSize);
+        int bucketCount = 0;
+        for (PlanAnalysis.Partition partition : partitions) {
+            bucketCount += partition.getBuckets().size();
+        }
+
+        assertEquals(expectedPartitionCount, partitions.size(), "Unexpected number of partitions");
+        assertEquals(expectedBucketCount, bucketCount, "Unexpected number of buckets");
+    }
+}
