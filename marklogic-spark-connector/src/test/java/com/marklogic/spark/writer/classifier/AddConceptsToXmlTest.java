698c7ea48375db2b985311cc6a645051bd75d2a3
Phil Barber <Phil.Barber@marklogic.com>
2025-01-16 15:32:22 -0500

    Change the switch for specifying the classifier protocol.

--- marklogic-spark-connector/src/test/java/com/marklogic/spark/writer/classifier/AddConceptsToXmlTest.java
+++ marklogic-spark-connector/src/test/java/com/marklogic/spark/writer/classifier/AddConceptsToXmlTest.java
@@ -5,14 +5,16 @@ package com.marklogic.spark.writer.classifier;
 
 import com.marklogic.junit5.XmlNode;
 import com.marklogic.spark.AbstractIntegrationTest;
+import com.marklogic.spark.ConnectorException;
 import com.marklogic.spark.Options;
+import org.apache.spark.sql.DataFrameWriter;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 class AddConceptsToXmlTest extends AbstractIntegrationTest {
 
@@ -31,7 +33,7 @@ class AddConceptsToXmlTest extends AbstractIntegrationTest {
             .option(Options.WRITE_SPLITTER_MAX_OVERLAP_SIZE, 0)
             .option(Options.WRITE_URI_TEMPLATE, "/split-test.xml")
             .option(Options.WRITE_CLASSIFIER_HOST, "demo.data.progress.cloud")
-            .option(Options.WRITE_CLASSIFIER_PROTOCOL, "https")
+            .option(Options.WRITE_CLASSIFIER_HTTPS, true)
             .option(Options.WRITE_CLASSIFIER_PORT, "443")
             .option(Options.WRITE_CLASSIFIER_ENDPOINT, "/cls/dev/cs1/")
             .option(Options.WRITE_CLASSIFIER_APIKEY, apiKey)
@@ -61,6 +63,27 @@ class AddConceptsToXmlTest extends AbstractIntegrationTest {
 
         XmlNode doc = readXmlDocument("/split-test.xml");
         doc.assertElementMissing("Expecting the chunk to not include a 'model:concepts' child element", "/root/model:chunks/model:chunk[1]/model:concepts");
+    }
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "SEMAPHORE_API_KEY", matches = ".*")
+    void noHttpsSpecifiedShouldDefaultToHttpAndFail() {
+        final String apiKey = System.getenv("SEMAPHORE_API_KEY");
+        assertNotNull(apiKey);
+
+        final DataFrameWriter<Row> dfw = readDocument("/marklogic-docs/java-client-intro.xml")
+            .write().format(CONNECTOR_IDENTIFIER)
+            .option(Options.CLIENT_URI, makeClientUri())
+            .option(Options.WRITE_SPLITTER_XPATH, "/root/text/text()")
+            .option(Options.WRITE_CLASSIFIER_HOST, "demo.data.progress.cloud")
+            .option(Options.WRITE_CLASSIFIER_PORT, "443")
+            .option(Options.WRITE_CLASSIFIER_ENDPOINT, "/cls/dev/cs1/")
+            .option(Options.WRITE_CLASSIFIER_APIKEY, apiKey)
+            .option(Options.WRITE_CLASSIFIER_TOKEN_ENDPOINT, "token/")
+            .mode(SaveMode.Append);
+
+        ConnectorException exception = assertThrowsConnectorException(dfw::save);
+        assertTrue(exception.getMessage().contains("Error retrieving token"), "Unexpected error: " + exception.getMessage());
     }
 
     private Dataset<Row> readDocument(String uri) {
