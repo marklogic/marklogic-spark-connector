046aebd2a63d5aa03591bcb6f66334b1ebb405b7
Rob Rudin <rudin@progress.com>
2025-11-14 14:00:43 -0500

    MLE-25288 Fixing bug with datatype and lang being set in MarkLogic 11
    Small fix, large test.

--- marklogic-spark-connector/src/test/java/com/marklogic/spark/writer/rdf/WriteTriplesTest.java
+++ marklogic-spark-connector/src/test/java/com/marklogic/spark/writer/rdf/WriteTriplesTest.java
@@ -3,13 +3,20 @@
  */
 package com.marklogic.spark.writer.rdf;
 
+import com.marklogic.client.io.Format;
+import com.marklogic.client.io.StringHandle;
+import com.marklogic.client.semantics.GraphManager;
 import com.marklogic.junit5.PermissionsTester;
+import com.marklogic.junit5.XmlNode;
 import com.marklogic.spark.ConnectorException;
 import com.marklogic.spark.Options;
 import org.apache.spark.sql.DataFrameWriter;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
+import org.jdom2.Namespace;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -24,6 +31,46 @@ class WriteTriplesTest extends AbstractWriteRdfTest {
 
         assertTripleCount(RdfRowConverter.DEFAULT_MARKLOGIC_GRAPH, 8,
             "Triples should be added to the default MarkLogic graph when the user does not specify a graph.");
+    }
+
+    @Test
+    void datatypeUsage() {
+        readRdfAndWrite("src/test/resources/rdf/datatype-test.ttl")
+            .option(Options.WRITE_PERMISSIONS, DEFAULT_PERMISSIONS)
+            .option(Options.WRITE_GRAPH, "datatype-graph")
+            .mode(SaveMode.Append)
+            .save();
+
+        List<String> uris = getUrisInCollection("datatype-graph", 2);
+        String triplesUri = uris.stream().filter(uri -> uri.startsWith("/triplestore")).findFirst().get();
+        XmlNode doc = readXmlDocument(triplesUri);
+
+        doc.assertElementExists(
+            "The Jane object doesn't have a language, so it should have a datatype and no xml:lang.",
+            "/sem:triples/sem:triple/sem:object[@datatype = 'http://www.w3.org/2001/XMLSchema#string' and . = 'Jane' and not(@xml:lang)]"
+        );
+        doc.assertElementExists(
+            "The Smith object does have a language, so it should have an xml:lang and no datatype. This avoids a bug " +
+                "in MarkLogic 11 where having both datatype and xml:lang results in an empty lang value. That bug " +
+                "does not occur in MarkLogic 12.",
+            "/sem:triples/sem:triple/sem:object[not(@datatype) and . = 'Smith' and @xml:lang='en-US']"
+        );
+        doc.assertElementExists(
+            "For a non-string value, a datatype should exist with no xml:lang.",
+            "/sem:triples/sem:triple/sem:object[@datatype = 'http://www.w3.org/2001/XMLSchema#integer' and . = '1111111111' and not(@xml:lang)]"
+        );
+
+        GraphManager graphManager = getDatabaseClient().newGraphManager();
+        String graphString = graphManager.things(new StringHandle().withFormat(Format.XML), "http://marklogicsparql.com/id#1111").get();
+        XmlNode graph = new XmlNode(graphString,
+            Namespace.getNamespace("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#"),
+            Namespace.getNamespace("ab", "http://marklogicsparql.com/addressbook#"));
+
+        // Verify that the "things" representation of the triples as XML matches the same conditions expressed above
+        // on the raw triples document.
+        graph.assertElementExists("//ab:firstName[@rdf:datatype='http://www.w3.org/2001/XMLSchema#string' and . = 'Jane' and not(@xml:lang)]");
+        graph.assertElementExists("//ab:lastName[@xml:lang='en-us' and . = 'Smith' and not(@datatype)]");
+        graph.assertElementExists("//ab:homeTel[@rdf:datatype='http://www.w3.org/2001/XMLSchema#integer' and . = '1111111111' and not(@xml:lang)]");
     }
 
     @Test
@@ -87,10 +134,14 @@ class WriteTriplesTest extends AbstractWriteRdfTest {
     }
 
     private DataFrameWriter<Row> readRdfAndWrite() {
+        return readRdfAndWrite("src/test/resources/rdf/mini-taxonomy.xml");
+    }
+
+    private DataFrameWriter<Row> readRdfAndWrite(String path) {
         return newSparkSession().read()
             .format(CONNECTOR_IDENTIFIER)
             .option(Options.READ_FILES_TYPE, "rdf")
-            .load("src/test/resources/rdf/mini-taxonomy.xml")
+            .load(path)
             .write()
             .format(CONNECTOR_IDENTIFIER)
             .option(Options.CLIENT_URI, makeClientUri());
