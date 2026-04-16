31a87a94e5bfe4c4faaab3bb017465a8433074e0
Rob Rudin <rudin@progress.com>
2026-04-16 15:24:18 -0400

    PDP-1213 Supporting a map of params for batch listener
    Batch listener can also optionally implement Closeable too.

--- marklogic-spark-connector/src/test/java/com/marklogic/spark/writer/WriteWithBatchListenerTest.java
+++ marklogic-spark-connector/src/test/java/com/marklogic/spark/writer/WriteWithBatchListenerTest.java
@@ -10,6 +10,8 @@ import com.marklogic.spark.Options;
 import org.apache.spark.sql.DataFrameWriter;
 import org.junit.jupiter.api.Test;
 
+import java.io.Closeable;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,15 +21,26 @@ class WriteWithBatchListenerTest extends AbstractWriteTest {
 
     @Test
     void simpleBatchListener() {
-        MyBatchListener.itemCount.set(0);
+        MyBatchListener.reset();
 
         newWriter()
             .option(Options.WRITE_BATCH_LISTENER_CLASSNAME, MyBatchListener.class.getName())
+            .option(Options.WRITE_BATCH_LISTENER_PARAM_PREFIX + "param1", "value1")
+            .option(Options.WRITE_BATCH_LISTENER_PARAM_PREFIX + "param2", "value2")
             .save();
 
         assertCollectionSize(COLLECTION, 200);
         assertEquals(200, MyBatchListener.itemCount.get(), "200 docs are written, and so are custom " +
             "batch listener should have an itemCount of 200.");
+
+        assertEquals(2, MyBatchListener.constructorParams.size(),
+            "The batch listener should have received the two parameters specified in the options");
+        assertEquals("value1", MyBatchListener.constructorParams.get("param1"));
+        assertEquals("value2", MyBatchListener.constructorParams.get("param2"));
+
+        assertTrue(MyBatchListener.wasClosed, "When the data writer has finished, it needs to see if " +
+            "the batch listener is not null and implements Closeable. If so, it should invoke close() " +
+            "on it so that the listener has a chance to clean up its resources.");
     }
 
     @Test
@@ -45,29 +58,54 @@ class WriteWithBatchListenerTest extends AbstractWriteTest {
     @Test
     void notABatchListener() {
         DataFrameWriter<?> writer = newWriter()
-            .option(Options.WRITE_BATCH_LISTENER_CLASSNAME, getClass().getName());
+            .option(Options.WRITE_BATCH_LISTENER_CLASSNAME, NotABatchListener.class.getName());
 
         ConnectorException ex = assertThrowsConnectorException(() -> writer.save());
-        assertTrue(ex.getMessage().contains("Failed to instantiate WriteBatchListener class"),
+        assertTrue(ex.getMessage().contains("does not implement WriteBatchListener"),
             "Should get an immediate failure if the class isn't actually a valid listener; actual error: " + ex.getMessage());
         assertCollectionSize(COLLECTION, 0);
     }
 
-    public static class MyBatchListener implements WriteBatchListener {
+    public static class MyBatchListener implements WriteBatchListener, Closeable {
 
         public static AtomicInteger itemCount = new AtomicInteger(0);
+        public static boolean wasClosed;
+        public static Map<String, String> constructorParams;
+
+        public static void reset() {
+            itemCount.set(0);
+            wasClosed = false;
+            constructorParams = null;
+        }
+
+        public MyBatchListener(Map<String, String> params) {
+            constructorParams = params;
+        }
 
         @Override
         public void processEvent(WriteBatch batch) {
             itemCount.addAndGet(batch.getItems().length);
         }
+
+        @Override
+        public void close() {
+            wasClosed = true;
+        }
     }
 
     public static class UnhappyBatchListener implements WriteBatchListener {
 
+        public UnhappyBatchListener(Map<String, String> params) {
+        }
+
         @Override
         public void processEvent(WriteBatch batch) {
             throw new RuntimeException("I am not happy.");
+        }
+    }
+
+    public static class NotABatchListener {
+        public NotABatchListener(Map<String, String> params) {
         }
     }
 }
